db33967253fae226f2d3115af2fe17dc90c82788
Zak Pacha <zak.pacha@gmail.com>
2016-02-06 18:49:43 +0000

    Good start to making a prototype -Put assets into .tmx map file -Map loaded in game -Working but slightly buggy collision detection

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -8,12 +8,32 @@ class HelloWorld : public cocos2d::Layer
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
+    
 
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
-
+    
+    bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event);
+    void keyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event *event);
+    void keyboardListener();
+    bool isKeyPressed(cocos2d::EventKeyboard::KeyCode);
+    double keyPressedDuration(cocos2d::EventKeyboard::KeyCode);
+    cocos2d::CCPoint tileCoordForPosition(cocos2d::CCPoint position);
+    
+    
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+    
+    cocos2d::CCPoint centerPoint;
+    cocos2d::CCTMXTiledMap* _tileMap;
+    cocos2d::CCTMXLayer* _background;
+    cocos2d::CCTMXLayer *_meta;
+    void setViewPointCenter(cocos2d::CCPoint position);
+    
+private:
+    cocos2d::CCSprite *_player;
+    bool isMoving;
+    
 };
 
 #endif // __HELLOWORLD_SCENE_H__
