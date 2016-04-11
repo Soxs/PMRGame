ce3e96fc4cc101309d002feb1aecc445cf610a2e
Zak Pacha <zak.pacha@gmail.com>
2016-04-11 17:36:08 +0100

    score flies to corner

--- Classes/ScoreManager.cpp
+++ Classes/ScoreManager.cpp
@@ -24,12 +24,12 @@ void ScoreManager::addToScore(int toadd) {
 void ScoreManager::addToScore(int toadd, cocos2d::Vec2 loc) {
     
     GameWorld* gameInstance = GameWorld::instance;
-    Label* tempScoreLabel = Label::createWithTTF("+" + std::to_string(toadd), "kenney-rocket.ttf", 12);
+    Label* tempScoreLabel = Label::createWithTTF("+" + std::to_string(toadd), "kenney-rocket.ttf", 16);
     tempScoreLabel->setColor(cocos2d::Color3B::RED);
     tempScoreLabel->enableOutline(Color4B(0,0,0,255),1);
     tempScoreLabel->setPosition(loc);
     gameInstance->addChild(tempScoreLabel, 1);
-    Sequence* anim = cocos2d::Sequence::create(cocos2d::MoveTo::create(1.0f, ccp(loc.x, loc.y + 50)), cocos2d::DelayTime::create(1.0f), cocos2d::RemoveSelf::create(), NULL);
+    Sequence* anim = cocos2d::Sequence::create(cocos2d::MoveTo::create(1.0f, Vec2(loc.x, loc.y + 50)), cocos2d::DelayTime::create(1.0f), cocos2d::MoveTo::create(0.5f, gameInstance->convertToNodeSpace(CCDirector::sharedDirector()->convertToGL(Vec2(110, 130)))), cocos2d::RemoveSelf::create(), NULL);
     tempScoreLabel->runAction(anim);
     score += toadd;
 }
